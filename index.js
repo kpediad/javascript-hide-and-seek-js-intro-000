cc4ef50ba875321448f22675eada921036acaab2
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-03-24 12:21:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function increaseRankBy(n) {
 
 function deepestChild() {
   let current = document.getElementById('grand-node')
-
+  debugger
   while (current) {
     if(!current.child) {
       return current

774ab79c393858cf41f70d02d8202a88898026b6
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-03-24 12:35:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  let current = document.querySelectorAll('grand-node')
+  let current = document.querySelectorAll('#grand-node')
   while (current) {
     if(!current.child) {
       return current
